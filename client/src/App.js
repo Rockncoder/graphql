5de9b00e739a686aeb91ce0b09e7e5b7db568a41
Troy Miles <rockncoder@gmail.com>
2018-09-06 14:41:21 -0700

    Adds the BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="main">
         <h1>Book List</h1>
+        <BookList/>
       </div>
     );
   }
