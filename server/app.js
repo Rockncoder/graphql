d326d45ee19730e4fe9d643d5874a6d6f295882e
Troy Miles <rockncoder@gmail.com>
2018-09-06 09:41:39 -0700

    Adds mongoose and connects to the db

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://user:test123@ds247852.mlab.com:47852/gql-ninja', {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 // middleware
 app.use('/graphql', graphqlHTTP({
@@ -14,5 +19,4 @@ app.use('/graphql', graphqlHTTP({
 // TODO: fix this hardcoded port
 app.listen(4000, () => {
   console.log('Now listening for requests on port 4000');
-
 });
